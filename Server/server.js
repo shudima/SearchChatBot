2d21b6e9dc2d4e009f9745e9b5c8046b324591da
unknown <DShulga@CZC1104JV3-VOC.sms1.local>
2015-02-25 19:22:57 +0200

    elastcsearch

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,28 @@
 var http = require('http');
-var elasticsearch = require('elasticsearch');
+ElasticSearchClient = require('elasticsearchclient');
 
-// Connect to localhost:9200 and use the default settings
-var client = new elasticsearch.Client();
+var serverOptions = {
+    host: 'localhost',
+    port: 9200
+};
+
+var elasticSearchClient = new ElasticSearchClient(serverOptions);
+
+var qryObj = {
+    'field' : 'term'
+}
+
+elasticSearchClient.search('my_index_name', 'my_type_name', qryObj)
+    .on('data', function(data) {
+        console.log(JSON.parse(data))
+    })
+    .on('done', function(){
+        //always returns 0 right now
+    })
+    .on('error', function(error){
+        console.log(error)
+    })
+    .exec()
 
 
 var server = http.createServer(function(req, res) {
@@ -11,4 +31,4 @@ var server = http.createServer(function(req, res) {
   res.end(JSON.stringify({ answer_text: "Hello from http" }));
 });
 console.log('Starts listening on 8080')
-server.listen(8080);
+server.listen(8180);
